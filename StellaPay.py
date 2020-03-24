8e2bfa08d0e737989e36b4cd78e04f4880270304
Yvo Elling <Yvo.elling@solarteameindhoven.nl>
2020-03-24 01:39:42 +0100

    Worked on multithreading to enable NFC reading on other thread

--- StellaPay.py
+++ StellaPay.py
@@ -1,9 +1,9 @@
 from kivy.app import App
 from kivy.config import Config
-from kivy.uix.label import Label
-from kivy.uix.image import *
-from PythonNFCReader import NFCReader as nfc
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 from scrs.DefaultScreen import DefaultScreen
+from scrs.WelcomeScreen import WelcomeScreen
 from kivy.core.window import Window
 
 import kivy
@@ -12,21 +12,25 @@ kivy.require('1.11.1')
 
 class StellaPay(App):
     def build(self):
-
-
-        # img = Image(source='img/STE.png')
-        # nfc_reader = nfc.NFCReader()
-        return DefaultScreen()
+        return sm
 
 
 if __name__ == '__main__':
     # Set fullscreen for application
-    Config.set('graphics', 'fullscreen', 'auto')
-    Config.set('graphics', 'window_state', 'maximized')
-    Config.write()
+    # Config.set('graphics', 'fullscreen', 'auto')
+    # Config.set('graphics', 'window_state', 'maximized')
+    # Config.write()
 
     # Set background image to match color of STE logo
     Window.clearcolor = (0.12549, 0.12549, 0.12549, 0)
+
+    #Load .kv file
+    Builder.load_file('kvs/screens.kv')
+
+    #Load screenloader and add screens
+    sm = ScreenManager()
+    sm.add_widget(DefaultScreen(name='DefaultScreen'))
+    sm.add_widget(WelcomeScreen(name='WelcomeScreen'))
 
     # run the application
     StellaPay().run()
--- scrs/DefaultScreen.py
+++ scrs/DefaultScreen.py
@@ -1,20 +1,30 @@
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.image import Image
+from kivy.uix.screenmanager import Screen
 from PythonNFCReader import NFCReader as nfc
 import threading
+
 
 # defaultScreen class
 # creates the default screen when the program waits for a card to be presented
 # changes when a card is presented
 #
-
-
-class DefaultScreen(AnchorLayout):
+class DefaultScreen(Screen):
+    uid = None
+    nfc_r = None
 
     def __init__(self, **kwargs):
         super(DefaultScreen, self).__init__(**kwargs)
-        img = Image(source="img/STE.png")
-        img.width
-        self.add_widget(img)
+
+        nfc_thread = threading.Thread(target=self.create_nfc_listener)
+        nfc_thread.start()
+
+        listener_thread = threading.Thread(target=self.create_thread_listener(nfc_thread))
+        listener_thread.start()
+
+    def create_nfc_listener(self):
+        self.nfc_r = nfc.NFCReader()
+
+    def create_thread_listener(self, nfc_thread):
+        nfc_thread.join()
+        self.uid = self.nfc_r.get_uid()
 
 
