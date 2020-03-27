054cb7d819da2fa2eb5ff9d752b2b2735a20266e
Yvo Elling <Yvo.elling@solarteameindhoven.nl>
2020-03-27 01:09:44 +0100

    Implemented transition to new screen after card is presented

--- StellaPay.py
+++ StellaPay.py
@@ -1,7 +1,7 @@
 from kivy.app import App
 from kivy.config import Config
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, FadeTransition
 from scrs.DefaultScreen import DefaultScreen
 from scrs.WelcomeScreen import WelcomeScreen
 from kivy.core.window import Window
@@ -17,9 +17,9 @@ class StellaPay(App):
 
 if __name__ == '__main__':
     # Set fullscreen for application
-    # Config.set('graphics', 'fullscreen', 'auto')
-    # Config.set('graphics', 'window_state', 'maximized')
-    # Config.write()
+    Config.set('graphics', 'fullscreen', 'auto')
+    Config.set('graphics', 'window_state', 'maximized')
+    Config.write()
 
     # Set background image to match color of STE logo
     Window.clearcolor = (0.12549, 0.12549, 0.12549, 0)
--- scrs/DefaultScreen.py
+++ scrs/DefaultScreen.py
@@ -1,4 +1,4 @@
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import Screen, SlideTransition
 from PythonNFCReader import NFCReader as nfc
 import threading
 
@@ -8,23 +8,49 @@ import threading
 # changes when a card is presented
 #
 class DefaultScreen(Screen):
-    uid = None
-    nfc_r = None
 
     def __init__(self, **kwargs):
+        # Call to super (Screen class)
         super(DefaultScreen, self).__init__(**kwargs)
 
-        nfc_thread = threading.Thread(target=self.create_nfc_listener)
+        # Declare member variables
+        # (Apparently Python wants them here)
+        self.nfc_r = None
+        self.nfc_uid = None
+
+        # Create and start a thread to listen for NFC card presentation
+        nfc_thread = threading.Thread(name="NFC_reader",
+                                      target=self.create_nfc_reader)
         nfc_thread.start()
 
-        listener_thread = threading.Thread(target=self.create_thread_listener(nfc_thread))
+        # Create and start a thread that monitors whether the nfc_thread
+        # has finished. This method is chosen to avoid having a blocking
+        # thread as the main UI thread
+        listener_thread = threading.Thread(name="NFC_reader_monitor",
+                                           target=self.launch_nfc_reader_monitor,
+                                           args=(nfc_thread, ))
         listener_thread.start()
 
-    def create_nfc_listener(self):
+    def create_nfc_reader(self):
+        # Create NFCReader() Object that waits until a card is presented
         self.nfc_r = nfc.NFCReader()
 
-    def create_thread_listener(self, nfc_thread):
+    #
+    # waits for the thread @nfc_thread to finish
+    # .join() is called from a seperate to avoiding a blocking main UI thread
+    #
+    def launch_nfc_reader_monitor(self, nfc_thread):
+        # Wait for the @nfc_thread thread to finish
         nfc_thread.join()
-        self.uid = self.nfc_r.get_uid()
+        # get UID for presented NFC card
+        self.nfc_uid = self.nfc_r.get_uid()
+
+        #Send UID to Django database to validate person
+        #placeholder
+
+        #Move to WelcomeScreen
+        self.manager.transition = SlideTransition(direction='left')
+        self.manager.current = 'WelcomeScreen'
+
 
 
