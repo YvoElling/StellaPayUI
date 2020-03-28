a09bf68be23bd0fd9840a7ffd7b7e961a6c5d987
Yvo Elling <Yvo.elling@solarteameindhoven.nl>
2020-03-28 17:57:49 +0100

    Added screen (also to screenmanager) and added Stella Pay to front

--- StellaPay.py
+++ StellaPay.py
@@ -4,6 +4,11 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, FadeTransition
 from scrs.DefaultScreen import DefaultScreen
 from scrs.WelcomeScreen import WelcomeScreen
+from scrs.ConfirmedScreen import ConfirmedScreen
+from scrs.CreditsScreen import CreditsScreen
+from scrs.ProductScreen import ProductScreen
+from scrs.ProfileScreen import ProfileScreen
+from scrs.RegisterUIDScreen import RegisterUIDScreen
 from kivy.core.window import Window
 
 import kivy
@@ -12,25 +17,31 @@ kivy.require('1.11.1')
 
 class StellaPay(App):
     def build(self):
-        return sm
+        return screen_manager
 
 
 if __name__ == '__main__':
     # Set fullscreen for application
-    Config.set('graphics', 'fullscreen', 'auto')
-    Config.set('graphics', 'window_state', 'maximized')
+    Config.set('graphics', 'width', '800')
+    Config.set('graphics', 'height', '480')
     Config.write()
 
     # Set background image to match color of STE logo
     Window.clearcolor = (0.12549, 0.12549, 0.12549, 0)
+    Window.size = (800, 480)
 
     # Load .kv file
     Builder.load_file('kvs/screens.kv')
 
     # Load screenloader and add screens
-    sm = ScreenManager()
-    sm.add_widget(DefaultScreen(name='DefaultScreen'))
-    sm.add_widget(WelcomeScreen(name='WelcomeScreen'))
+    screen_manager = ScreenManager()
+    screen_manager.add_widget(DefaultScreen(name='DefaultScreen'))
+    screen_manager.add_widget(WelcomeScreen(name='WelcomeScreen'))
+    screen_manager.add_widget(ConfirmedScreen(name='ConfirmedScreen'))
+    screen_manager.add_widget(CreditsScreen(name="CreditsScreen"))
+    screen_manager.add_widget(ProductScreen(name="ProductScreen"))
+    screen_manager.add_widget(ProfileScreen(name="ProfileScreen"))
+    screen_manager.add_widget(RegisterUIDScreen(name="RegisterUIDScreen"))
 
     # run the application
     StellaPay().run()
---
+++ scrs/ProductScreen.py
@@ -0,0 +1,5 @@
+from kivy.uix.screenmanager import Screen
+
+
+class ProductScreen(Screen):
+    pass
