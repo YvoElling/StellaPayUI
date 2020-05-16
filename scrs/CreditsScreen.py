5dbceb37d96556a132b742b4025f74f4d624869f
Yvo Elling <Yvo.elling@solarteameindhoven.nl>
2020-05-16 19:19:17 +0200

    Removed time-outs for demo purpose

--- scrs/CreditsScreen.py
+++ scrs/CreditsScreen.py
@@ -11,39 +11,9 @@ class CreditsScreen(Screen):
         # call to user with arguments
         super(CreditsScreen, self).__init__(**kwargs)
 
-        # define time_out event
-        self.timeout_event = None
-
     #
     # used to return to the welcome pages. Callback function from stop button
     #
     def on_stop(self):
         self.manager.transition = CardTransition(direction="up", mode="push")
         self.manager.current = 'DefaultScreen'
-
-    #
-    # Called on time_out, forces a @on_stop() function call
-    #
-    def on_timeout(self, dt):
-        self.on_stop()
-
-    #
-    # Upon entering the creditsScreen, start the time-out counter
-    #
-    def on_enter(self, *args):
-        timeout_time = 30  # seconds
-        self.timeout_event = Clock.schedule_once(self.on_timeout, timeout_time)
-
-    #
-    #
-    #
-    def on_touch_up(self, touch):
-        timeout_time = 30 # seconds
-        Clock.unschedule(self.timeout_event)
-        self.timeout_event = Clock.schedule_once(self.on_timeout, timeout_time)
-
-    #
-    # Upon leaving this screen, stop the timeout function.
-    #
-    def on_leave(self, *args):
-        Clock.unschedule(self.timeout_event)
