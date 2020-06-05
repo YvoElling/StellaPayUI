bfae5da6e1439fd10e49029b5428f0893a9c2a3d
Yvo Elling <Yvo.elling@solarteameindhoven.nl>
2020-06-05 20:29:11 +0200

    Added auto restart and update upon turning off device

---
+++ autostart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Ping google DNS until connection is made upon startup
+while ! timeout 0.2 ping -c 1 -n 8.8.8.8 &> /dev/null
+do
+    printf "Connecting..."
+done
+printf "Connected!"
+
+# Change to the StellaPayUI directory
+cd /home/pi/StellaPayUI || cd /home/yvoelling/Repositories/Python/StellaPayUI || exit
+
+# Launch git pull to update the running StellaPay version to the latest available on the master
+git pull
+
+# Start the StellaPay program
+python3 StellaPay.py
+exec bash
+
+
+
