b56fd8efac4fda19dd289055331890efc6971ca3
Yvo Elling <Yvo.elling@solarteameindhoven.nl>
2020-08-01 18:43:03 +0200

    Implemented profileScreen timeouts

--- scrs/ProfileScreen.py
+++ scrs/ProfileScreen.py
@@ -1,5 +1,6 @@
+from kivy.clock import Clock
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen, SlideTransition
+from kivy.uix.screenmanager import Screen
 
 
 class ProfileScreen(Screen):
@@ -9,14 +10,29 @@ class ProfileScreen(Screen):
     def __init__(self, **kwargs):
         super(ProfileScreen, self).__init__(**kwargs)
 
+        # timeout variables
+        self.timeout_event = None
+        self.timeout_time = 45  # Seconds
+
     # Called when the screen is loaded:
     # - for retrieving user name
     def on_enter(self, *args):
         self.ids.username.text = self.manager.get_screen("DefaultScreen").user_name
+        self.timeout_event = Clock.schedule_once(self.on_timeout, self.timeout_time)
 
     # Return to the product page
     def on_back(self):
         self.manager.current = 'ProductScreen'
+        self.timeout_event.cancel()
+
+    # Return to defaultScreen upon timeout
+    def on_timeout(self, dt):
+        self.manager.current = 'DefaultScreen'
+
+    # Reset timer when the screen is touched
+    def on_touch_up(self, touch):
+        self.timeout_event.cancel()
+        self.timeout_event = Clock.schedule_once(self.on_timeout, self.timeout_time)
 
     # Clear name on leave
     def on_leave(self, *args):
